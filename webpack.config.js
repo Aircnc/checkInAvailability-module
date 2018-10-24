2605d60d2523ab2f65ac3ad2b3b68038aeec5cfc
Boyun Zou <zouboyun@Boyuns-MacBook-Pro.local>
2018-10-24 16:33:26 -0700

    set up dev dependencies

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+var SRC_DIR = path.join(__dirname, '/client/src');
+var DIST_DIR = path.join(__dirname, '/client/dist');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    filename: 'aval.bundle.js',
+    path: DIST_DIR
+  },
+  module : {
+    rules : [
+      {
+        test : [/\.js$/, /\.jsx?$/],
+        loader : 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react']
+        }
+      }
+    ]
+  }
+};

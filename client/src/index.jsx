6d0326f8d5a2aeaaa1c4af7eb3fc1c8cdc2decc4
Boyun Zou <zouboyun@Boyuns-MacBook-Pro.local>
2018-10-29 17:55:39 -0700

    cleaned index jsx code

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,18 +4,6 @@ import Nav from './components/Nav';
 import Images from './components/Images';
 import MainBody from './components/MainBody';
 
-// class App extends React.Component {
-//     constructor(props) {
-//         super(props);
-//     }
-
-//     render() {
-//         return (
-//             <Nav />
-//         )
-//     }
-// }
-
 ReactDOM.render(<Nav />, document.getElementById('nav'));
 ReactDOM.render(<Images />, document.getElementById('images'));
 ReactDOM.render(<MainBody />, document.getElementById('main-body'));

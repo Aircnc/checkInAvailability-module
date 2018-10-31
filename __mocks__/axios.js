13ecf602580218592c958f1c4cf335ec1038cd31
Boyun Zou <zouboyun@Boyuns-MacBook-Pro.local>
2018-10-31 16:00:12 -0700

    ignored database tests

--- __mocks__/axios.js
+++ __mocks__/axios.js
@@ -3,7 +3,7 @@ const reservations = require('./data/reservations');
 const axios = jest.genMockFromModule('axios');
 
 axios.get = url => new Promise((resolve, reject) => {
-  if (url === '/listings/7/reservations') {
+  if (url) {
     resolve(reservations);
   } else {
     reject(new Error('invalid url.'));

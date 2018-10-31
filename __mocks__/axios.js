809c172033cde98091a3a6711add0736ba585807
Boyun Zou <zouboyun@Boyuns-MacBook-Pro.local>
2018-10-31 15:39:17 -0700

    updated axios mock function

--- __mocks__/axios.js
+++ __mocks__/axios.js
@@ -2,15 +2,13 @@ const reservations = require('./data/reservations');
 
 const axios = jest.genMockFromModule('axios');
 
-axios.get = (url) => {
-  return new Promise((resolve, reject) => {
-    if (url === '/listings/7/reservations') {
-      resolve(reservations);
-    } else {
-      reject(new Error('invalid url.'));
-    }
-  });
-};
+axios.get = url => new Promise((resolve, reject) => {
+  if (url === '/listings/7/reservations') {
+    resolve(reservations);
+  } else {
+    reject(new Error('invalid url.'));
+  }
+});
 
 
 module.exports = axios;
